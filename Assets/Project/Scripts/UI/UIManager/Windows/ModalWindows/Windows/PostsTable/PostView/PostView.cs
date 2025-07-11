550342fd2bdd8eaed33cf40540ca732c44a80950
Leonid Tsypkin <caudate7855@gmail.com>
2025-07-11 16:55:18 +0700

    - fix: post view minor bug fixed

--- Assets/Project/Scripts/UI/UIManager/Windows/ModalWindows/Windows/PostsTable/PostView/PostView.cs
+++ Assets/Project/Scripts/UI/UIManager/Windows/ModalWindows/Windows/PostsTable/PostView/PostView.cs
@@ -1,20 +1,21 @@
+using TMPro;
 using UnityEngine;
 
 namespace UI
 {
     public class PostView : MonoBehaviour
     {
-        private int _userId;
-        private int _id;
-        private string _title;
-        private string _body;
+        [SerializeField] private TMP_Text _userId;
+        [SerializeField] private TMP_Text _id;
+        [SerializeField] private TMP_Text _title;
+        [SerializeField] private TMP_Text _body;
 
         public void InitializeData(int userId, int id, string title, string body)
         {
-            _userId = userId;
-            _id = id;
-            _title = title;
-            _body = body;
+            _userId.text = userId.ToString();
+            _id.text = id.ToString();
+            _title.text = title;
+            _body.text = body;
         }
     }
 }
